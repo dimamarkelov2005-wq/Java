6ee27842b04a0074e5434d9c30f81f0915b0b981
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-01-22 14:36:24 +0500

    Create Main.java

---
+++ lab1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StudentInfo student = new StudentInfo(
+                "Маркелов Дмитрий Сергеевич",
+                "ИВТ-21",
+                "Информационные технологии"
+        );
+
+        student.printInfo();
+    }
+}
