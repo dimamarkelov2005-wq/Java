8227db56de66ff8774fd48a791bc7e22b8e5f17c
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-05 13:55:36 +0500

    Create ExecutionOrder.java

---
+++ lab3/main/src/ExecutionOrder.java
@@ -0,0 +1,17 @@
+package linear;
+
+public class ExecutionOrder {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 7;
+        int c = 10;
+
+        int sum = a + b + c;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("Сумма: " + sum);
+    }
+}
