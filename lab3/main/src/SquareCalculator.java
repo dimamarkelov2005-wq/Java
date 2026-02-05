6c56c8d9264bb59019cb2073b44113e6680ef089
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-05 13:56:55 +0500

    Create SquareCalculator.java

---
+++ lab3/main/src/SquareCalculator.java
@@ -0,0 +1,18 @@
+package linear;
+
+import java.util.Scanner;
+
+public class SquareCalculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите целое число: ");
+        int number = scanner.nextInt();
+
+        int square = number * number;
+        System.out.println("Квадрат числа: " + square);
+
+        scanner.close();
+    }
+}
