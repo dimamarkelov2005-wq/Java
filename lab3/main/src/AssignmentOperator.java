b7ef4fedc0d6ed83f11890265bf956e327200d4d
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-05 13:56:31 +0500

    Create AssignmentOperator.java

---
+++ lab3/main/src/AssignmentOperator.java
@@ -0,0 +1,12 @@
+package linear;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+
+        int value = 20;
+        System.out.println("Исходное значение: " + value);
+
+        value = value + 5;
+        System.out.println("Новое значение: " + value);
+    }
+}
