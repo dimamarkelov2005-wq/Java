df455c5e2b000965c3be4e545ab49c367eeccb19
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-05 13:57:59 +0500

    Create SumAndAverage.java

---
+++ lab3/main/src/SumAndAverage.java
@@ -0,0 +1,24 @@
+package linear;
+
+import java.util.Scanner;
+
+public class SumAndAverage {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double b = scanner.nextDouble();
+
+        double sum = a + b;
+        double average = sum / 2;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Среднее арифметическое: " + average);
+
+        scanner.close();
+    }
+}
