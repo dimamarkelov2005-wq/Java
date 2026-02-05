e94d397de6a3cfceeac700cb2b5758e0048e0fbe
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-05 13:56:03 +0500

    Create ArithmeticOperations.java

---
+++ lab3/main/src/ArithmeticOperations.java
@@ -0,0 +1,13 @@
+package linear;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+        int x = 12;
+        int y = 4;
+
+        System.out.println("Сложение: " + (x + y));
+        System.out.println("Умножение: " + (x * y));
+        System.out.println("Деление: " + ((double) x / y));
+    }
+}
