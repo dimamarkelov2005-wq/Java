e26add0d40553e2ce3f1e1bac08db026657bc532
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-05 13:59:31 +0500

    Create CelsiusToFahrenheit.java

---
+++ lab3/main/src/CelsiusToFahrenheit.java
@@ -0,0 +1,18 @@
+package linear;
+
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите температуру в Цельсиях: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = celsius * 9 / 5 + 32;
+        System.out.println("Температура в Фаренгейтах: " + fahrenheit);
+
+        scanner.close();
+    }
+}
