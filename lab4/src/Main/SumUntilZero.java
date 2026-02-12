bc80c8bfd4deb67ce7d0426f6fb2aa164e7d9613
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-12 13:32:26 +0500

    Add SumUntilZero program to calculate sum until zero

---
+++ lab4/src/Main/SumUntilZero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int number;
+
+        do {
+            number = scanner.nextInt();
+            sum += number;
+        } while (number != 0);
+
+        System.out.println("Сумма: " + sum);
+        scanner.close();
+    }
+}
