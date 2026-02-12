06ed4d7eb5f0ba67372d8414aa6e10f0b73a816b
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-12 13:33:29 +0500

    Add CompareNumbers class to compare two integers

---
+++ lab4/src/Main/CompareNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a > b)
+            System.out.println("Первое больше");
+        else if (a < b)
+            System.out.println("Второе больше");
+        else
+            System.out.println("Равны");
+
+        scanner.close();
+    }
+}
