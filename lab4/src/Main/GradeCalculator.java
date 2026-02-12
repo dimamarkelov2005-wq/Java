694dd0635fe3f6f5bcef646b9d7972b39cc6b26f
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-02-12 13:33:10 +0500

    Add GradeCalculator to determine grades based on score

---
+++ lab4/src/Main/GradeCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+
+        if (score >= 90)
+            System.out.println("5");
+        else if (score >= 75)
+            System.out.println("4");
+        else if (score >= 50)
+            System.out.println("3");
+        else
+            System.out.println("2");
+
+        scanner.close();
+    }
+}
