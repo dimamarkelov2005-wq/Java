f51e1f5d7914b66190c91e0ddeffce9fd4c70aa4
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-02 13:38:22 +0500

    Create Task5_PowerCalculator.java

---
+++ lab11/src/Main/Task5_PowerCalculator.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PowerCalculator {
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.pow(sc.nextDouble(), sc.nextDouble()));
+    }
+}
