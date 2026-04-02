f5b8794b2b41daa1b5a73cf3c2c27afc1eea2bd5
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-02 13:39:07 +0500

    Create Task6_SqrtCalculator.java

---
+++ lab11/src/Main/Task6_SqrtCalculator.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class SqrtCalculator {
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.sqrt(sc.nextDouble()));
+    }
+}
