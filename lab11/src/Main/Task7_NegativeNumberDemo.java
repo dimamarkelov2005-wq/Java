296f0d1efc47297d7a202be853eb2289631f23fa
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-02 13:39:37 +0500

    Add NegativeNumberDemo class for input handling

---
+++ lab11/src/Main/Task7_NegativeNumberDemo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class NegativeNumberDemo {
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        System.out.println("Number: " + x);
+    }
+}
