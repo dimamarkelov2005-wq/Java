374b25dfea881994968c7de193e1cc7a6a466e68
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-09 14:04:41 +0500

    Create Task17_RemoveDigits.java

---
+++ lab12/src/Main/Task17_RemoveDigits.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Task17_RemoveDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().replaceAll("\\d", ""));
+    }
+}
