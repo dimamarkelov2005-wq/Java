2c1ee6d1a59e1ac6a468b0ae8c9a55f81514eac2
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-09 14:05:53 +0500

    Create Task20_OnlyDigits.java

---
+++ lab12/src/Main/Task20_OnlyDigits.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Task20_OnlyDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().matches("\\d+"));
+    }
+}
