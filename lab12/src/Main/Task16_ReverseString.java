7443ef530a884787c4a2bfa7f130155838aa55ac
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-09 14:04:22 +0500

    Create Task16_ReverseString.java

---
+++ lab12/src/Main/Task16_ReverseString.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Task16_ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(new StringBuilder(sc.nextLine()).reverse());
+    }
+}
