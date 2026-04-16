03b1f559ad3e21d7b681cc33921c500fe89233d4
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-16 13:31:58 +0500

    Create Task7_Capitalize.java

---
+++ lab13/src/Main/Task7_Capitalize.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task7_Capitalize {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        System.out.println(s.substring(0,1).toUpperCase() + s.substring(1));
+    }
+}
