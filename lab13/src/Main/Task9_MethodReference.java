6603ae82ded07bba87ac4f5025baf3d07d18f675
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-16 13:32:42 +0500

    Create Task9_MethodReference.java

---
+++ lab13/src/Main/Task9_MethodReference.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Task9_MethodReference {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> list = Arrays.stream(sc.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+        list.forEach(System.out::println);
+    }
+}
