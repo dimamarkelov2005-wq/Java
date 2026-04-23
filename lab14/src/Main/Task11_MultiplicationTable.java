33454a610530f7a86c5497122dde17d81ab984d5
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-23 14:02:02 +0500

    Create Task11_MultiplicationTable.java

---
+++ lab14/src/Main/Task11_MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task11_MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число для таблицы умножения: ");
+        int n = scanner.nextInt();
+        int i = 1;
+        do {
+            System.out.println(n + " * " + i + " = " + (n * i));
+            i++;
+        } while (i <= 10);
+    }
+}
