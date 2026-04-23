ec83b59af8e1c0519878eaafab81489939055529
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-23 13:49:45 +0500

    Create Task4_PasswordCheck.java

---
+++ lab14/src/Main/Task4_PasswordCheck.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task4_PasswordCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String correctPassword = "12345";
+        String input;
+        do {
+            System.out.println("Введите пароль:");
+            input = scanner.nextLine();
+        } while (!input.equals(correctPassword));
+        System.out.println("Доступ разрешён!");
+    }
+}
