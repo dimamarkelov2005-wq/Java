c10e07674a86dd07c1de65ac5215db84b93757ca
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-23 13:48:50 +0500

    Create Task1_Numbers1to10.java

---
+++ lab14/src/Main/Task1_Numbers1to10.java
@@ -0,0 +1,9 @@
+public class Task1_Numbers1to10 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
