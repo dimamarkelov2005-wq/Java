538ba8bb5e64968a492a412ee56354315d2418b3
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-23 13:50:19 +0500

    Create Task6_ContinueOrStop.java

---
+++ lab14/src/Main/Task6_ContinueOrStop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task6_ContinueOrStop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+        do {
+            System.out.println("Выполнение операции...");
+            System.out.println("Продолжить? (yes/no):");
+            choice = scanner.nextLine();
+        } while (choice.equalsIgnoreCase("yes"));
+        System.out.println("Программа завершена.");
+    }
+}
