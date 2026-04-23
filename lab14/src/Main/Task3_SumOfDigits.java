fffddf657e0da19ff86974614a791bd5896c4e2f
Dimitry Markelov <dimamarkelov2005@gmail.com>
2026-04-23 13:49:27 +0500

    Create Task3_SumOfDigits.java

---
+++ lab14/src/Main/Task3_SumOfDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task3_SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = Math.abs(scanner.nextInt());
+        int sum = 0;
+        do {
+            sum += number % 10;
+            number /= 10;
+        } while (number > 0);
+        System.out.println("Сумма цифр: " + sum);
+    }
+}
